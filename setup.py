330c96418a87da861b4b83a2431bd27ee773eff3
Yuzuki Fujita <fujifujiyuzunoki@icloud.com>
2025-11-13 17:47:36 +0900

    Initial commit3

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Yuzuki Fujita',
+    maintainer_email='fujifujiyuzunoki@icloud.com',
+    description='ロボットシステム学Ros2',
+    license='BSD-3-Clause',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
