21a8616e837693332dcf95e4ae2d643f80c7d77e
Yuzuki Fujita <fujifujiyuzunoki@icloud.com>
2025-12-04 12:21:55 +0900

    Add correct source code

---
+++ mypkg/talker.py
@@ -0,0 +1,20 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+rclpy.init()
+node = Node("talker")
+
+
+def cb(request, response):
+    if request.name == "藤田柚樹":
+        response.age = 20
+    else:
+        response.age = 255
+
+    return response
+
+
+def main():
+    srv = node.create_service(Query, "query", cb)
+    rclpy.spin(node)
